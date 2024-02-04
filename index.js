b422136c6dc3a7a51ca27d6f372974248ee520fa
Nooralrihany <Nooralrihany@gmail.com>
2024-02-05 00:31:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var myName = 'noor'
+console.log(typeof myName);
+var number = 23
+console.log(typeof number)
+var isStudent = true
+console.log(typeof isStudent)
+
+var myName ='noor'
+var greeting ='hello'
+console.log(greeting , myName)
+var number1 = '10'
+var number2 = '20'
+console.log( 'sum', number1 + number2);
